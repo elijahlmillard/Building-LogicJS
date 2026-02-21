063bd59a82631ac8b5791e3b0e3755962123c8e5
Elijah Millard <emillard@icstars.org>
2026-02-20 21:39:57 -0600

    Scaffold for loop
    add switch and cases for loop logic

--- UnderstandingJS-Katas/kata2.js
+++ UnderstandingJS-Katas/kata2.js
@@ -4,4 +4,19 @@
 // Commit 2: "added fizzbuzz logic"
 // Commit 3: "refactored with clear variable and comments"
 
+for (i = 1; i < 26; i++) {
 
+  switch (true){
+
+    case i % 3 && i % 5 === 0:
+      break;
+
+    case i % 3 === 0:
+      break;
+
+    case i % 5 === 0:
+      break;
+
+    default:
+  }
+}
