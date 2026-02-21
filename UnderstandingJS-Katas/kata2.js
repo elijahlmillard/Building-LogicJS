18bdf7d17ce3b59af311668efab866931cff38c7
Elijah Millard <emillard@icstars.org>
2026-02-20 21:40:38 -0600

    add fizzbuzz
    console now logs fizzbuzz

--- UnderstandingJS-Katas/kata2.js
+++ UnderstandingJS-Katas/kata2.js
@@ -9,14 +9,18 @@ for (i = 1; i < 26; i++) {
   switch (true){
 
     case i % 3 && i % 5 === 0:
+        console.log("FizzBuzz")
       break;
 
     case i % 3 === 0:
+        console.log("fizz");
       break;
 
     case i % 5 === 0:
+        console.log("Buzz");
       break;
 
     default:
+      console.log(i);
   }
 }
