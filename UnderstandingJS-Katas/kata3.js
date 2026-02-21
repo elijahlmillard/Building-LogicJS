281d5d34a000296ff65a5feebe7046336b446a17
Elijah Millard <emillard@icstars.org>
2026-02-20 21:48:53 -0600

    start from scratch
    remove old nested loop

--- UnderstandingJS-Katas/kata3.js
+++ UnderstandingJS-Katas/kata3.js
@@ -4,13 +4,7 @@
 // Commit 2: "added multiplication logic"
 // Commit 3: "refactored for readability and spacing"
 
-for (let i = 1; i <= 5; i++) {
-  let row = "";
-  for (let j = 1; j <= 5; j++) {
-    row += (i * j) + "\t";
-  }
-  console.log(row);
-}
+
 
 // Stretch Goal: Guard clause example
 function printNumber(num) {
